806e366c0ed9b4207fa22ffdaea3262a5a829178
andybets <andyb@datadevil.co.uk>
2023-08-12 16:27:06 +0100

    Added item categories table and tidying

--- include/dohdocs.hpp
+++ include/dohdocs.hpp
@@ -15,19 +15,19 @@ namespace eosio {
          // adds a candidate tooltip document for the faction and language specified
          // the author must be a registered author for the faction and language specified
          // author auth required, and author pays for multiindex ram
-         ACTION adddoc(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name author, const std::string title, const std::string content);
+         ACTION adddoc(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const uint64_t category_id, const name author, const std::string title, const std::string content);
 
          // deletes a candidate tooltip document for the faction and language specified
          // only the author of the document, or the editor for the faction/language may delete it
-         ACTION deldoc(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name author);
+         ACTION deldoc(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name author);
 
          // publishes a candidate tooltip document for the faction and language specified, and copies it to the published table
          // the editor must be a registered editor for the faction specified
          // editor pays for multiindex ram
-         ACTION publish(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name editor);
+         ACTION publish(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name editor);
 
          // unpublishes a candidate tooltip document for the faction and language specified, and removes it from the published table
-         ACTION unpublish(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name editor);
+         ACTION unpublish(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name editor);
 
          // registers an author who can submit any document for the faction and language specified
          // an author may be registered several times for different factions/languages
@@ -47,8 +47,16 @@ namespace eosio {
          // requires contract authorisation
          ACTION deleditor(const name editor, const uint32_t faction_id);
 
-         static uint128_t composite_key_128(const uint64_t &doc_id, const uint32_t &faction_id, const uint32_t &language_id) {
-             return (uint128_t{doc_id} << 64) | (uint128_t{faction_id} << 32) | language_id;
+         // adds item category
+         // requires contract authorisation
+         ACTION setcategory(const uint64_t category_id, const std::string name, const std::string description);
+
+         // deleted category
+         // requires contract authorisation
+         ACTION delcategory(const uint64_t category_id);
+
+         static uint128_t composite_key_128(const uint64_t &item_id, const uint32_t &faction_id, const uint32_t &language_id) {
+             return (uint128_t{item_id} << 64) | (uint128_t{faction_id} << 32) | language_id;
          }
 
       private:
@@ -56,29 +64,40 @@ namespace eosio {
          /* stores documents which have been submitted, but not yet approved */
          TABLE candidatedoc {
             uint64_t      id;
-            uint64_t      doc_id;
+            uint64_t      item_id;
             uint32_t      faction_id;
             uint32_t      language_id;
+            uint64_t      category_id;
             name          author;
             std::string   title;
             std::string   content;
             uint64_t primary_key() const { return id; }
-            uint128_t by_composite_key() const { return composite_key_128(doc_id, faction_id, language_id); }
+            uint128_t by_composite_key() const { return composite_key_128(item_id, faction_id, language_id); }
          };
 
          /* stores documents which have been approved by the relevant editor for the faction */
          TABLE  publisheddoc {
             uint64_t      id;
-            uint64_t      doc_id;
+            uint64_t      item_id;
             uint32_t      faction_id;
             uint32_t      language_id;
+            uint64_t      category_id;
             name          author;
             std::string   title;
             std::string   content;
             time_point    approved_at;
             name          approved_by;
             uint64_t primary_key() const { return id; }
-            uint128_t by_composite_key() const { return composite_key_128(doc_id, faction_id, language_id); }
+            uint128_t by_composite_key() const { return composite_key_128(item_id, faction_id, language_id); }
+            uint64_t by_category() const { return category_id; }
+         };
+
+         /* stores the list of item categories */
+         TABLE category {
+            uint64_t      id;
+            std::string   name;
+            std::string   description;
+            uint64_t primary_key() const { return id; }
          };
 
          /* stores the list of authors permitted to submit documents */
@@ -104,7 +123,10 @@ namespace eosio {
             indexed_by<"composite"_n, const_mem_fun<candidatedoc, uint128_t, &candidatedoc::by_composite_key> > > candidatedocs_table;
 
          typedef eosio::multi_index< "published"_n, publisheddoc,
-            indexed_by<"composite"_n, const_mem_fun<publisheddoc, uint128_t, &publisheddoc::by_composite_key> > > publisheddocs_table;
+             indexed_by<"composite"_n, const_mem_fun<publisheddoc, uint128_t, &publisheddoc::by_composite_key>>,
+             indexed_by<"bycategory"_n, const_mem_fun<publisheddoc, uint64_t, &publisheddoc::by_category>> > publisheddocs_table;
+
+         typedef eosio::multi_index< "categories"_n, category > categories_table;
 
          typedef eosio::multi_index< "authors"_n, author,
             indexed_by<"composite"_n, const_mem_fun<author, uint128_t, &author::by_composite_key> > > authors_table;
--- src/dohdocs.cpp
+++ src/dohdocs.cpp
@@ -2,7 +2,7 @@
 
 namespace eosio {
 
-    ACTION dohdocs::adddoc(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name author, const std::string title, const std::string content) {
+    ACTION dohdocs::adddoc(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const uint64_t category_id, const name author, const std::string title, const std::string content) {
         require_auth(author);
         
         authors_table authors(get_self(), get_self().value);
@@ -12,27 +12,28 @@ namespace eosio {
         candidatedocs_table docs(get_self(), get_self().value);
         docs.emplace(author, [&](auto& row) {
             row.id = docs.available_primary_key();
-            row.doc_id = doc_id;
+            row.item_id = item_id;
             row.faction_id = faction_id;
             row.language_id = language_id;
+            row.category_id = category_id;
             row.author = author;
             row.title = title;
             row.content = content;
         });
     }
 
-    ACTION dohdocs::deldoc(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name author) {
+    ACTION dohdocs::deldoc(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name author) {
         require_auth(author);
         
         candidatedocs_table docs(get_self(), get_self().value);
-        auto doc_key = composite_key_128(doc_id, faction_id, language_id);
+        auto doc_key = composite_key_128(item_id, faction_id, language_id);
         auto doc_itr = docs.get(doc_key, "Document not found");
         eosio::check(doc_itr.author == author, "Only the author or editor can delete the document");
 
         docs.erase(doc_itr);
     }
 
-    ACTION dohdocs::publish(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name editor) {
+    ACTION dohdocs::publish(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name editor) {
         require_auth(editor);
         
         editors_table editors(get_self(), get_self().value);
@@ -40,15 +41,16 @@ namespace eosio {
         eosio::check(editor_itr != editors.end(), "Editor not registered");
 
         candidatedocs_table candidate_docs(get_self(), get_self().value);
-        auto candidate_key = composite_key_128(doc_id, faction_id, language_id);
+        auto candidate_key = composite_key_128(item_id, faction_id, language_id);
         auto candidate_itr = candidate_docs.get(candidate_key, "Document not found");
 
         publisheddocs_table published_docs(get_self(), get_self().value);
         published_docs.emplace(editor, [&](auto& row) {
             row.id = published_docs.available_primary_key();
-            row.doc_id = doc_id;
+            row.item_id = item_id;
             row.faction_id = faction_id;
             row.language_id = language_id;
+            row.category_id = candidate_itr.category_id;
             row.author = candidate_itr.author;
             row.title = candidate_itr.title;
             row.content = candidate_itr.content;
@@ -56,11 +58,9 @@ namespace eosio {
             row.approved_by = editor;
         });
 
-        // todo - change to just mark the candidate doc as approved rather than deleting it
-        candidate_docs.erase(candidate_itr);
     }
 
-    ACTION dohdocs::unpublish(const uint64_t doc_id, const uint32_t faction_id, const uint32_t language_id, const name editor) {
+    ACTION dohdocs::unpublish(const uint64_t item_id, const uint32_t faction_id, const uint32_t language_id, const name editor) {
         require_auth(editor);
         
         editors_table editors(get_self(), get_self().value);
@@ -68,7 +68,7 @@ namespace eosio {
         eosio::check(editor_itr != editors.end(), "Editor not registered");
 
         publisheddocs_table published_docs(get_self(), get_self().value);
-        auto pub_key = composite_key_128(doc_id, faction_id, language_id);
+        auto pub_key = composite_key_128(item_id, faction_id, language_id);
         auto pub_itr = published_docs.get(pub_key, "Document not found");
 
         published_docs.erase(pub_itr);
@@ -124,4 +124,40 @@ namespace eosio {
 
         editors.erase(editor_itr);
     }
+
+    ACTION dohdocs::setcategory(const uint64_t category_id, const std::string name, const std::string description) {
+        require_auth(get_self()); // Ensure that only the contract account can call this action.
+        
+        categories_table categories(get_self(), get_self().value); // Get the table for categories.
+        
+        auto cat_itr = categories.find(category_id); // Try to find the category by its ID.
+
+        if (cat_itr == categories.end()) {
+            // The category doesn't exist yet, so create a new one.
+            categories.emplace(get_self(), [&](auto& row) {
+                row.id = category_id;
+                row.name = name;
+                row.description = description;
+            });
+        } else {
+            // The category exists. We will update it.
+            categories.modify(cat_itr, get_self(), [&](auto& row) {
+                row.name = name;
+                row.description = description;
+            });
+        }
+    }
+
+    ACTION dohdocs::delcategory(const uint64_t category_id) {
+        require_auth(get_self()); // Ensure that only the contract account can call this action.
+        
+        categories_table categories(get_self(), get_self().value); // Get the table for categories.
+        
+        auto cat_itr = categories.find(category_id); // Try to find the category by its ID.
+        eosio::check(cat_itr != categories.end(), "Category not found"); // If the category doesn't exist, throw an error.
+
+        categories.erase(cat_itr); // Remove the category from the table.
+    }
+
+
 }
